90723718d711f7af1d1582ab922f96cd2a867bf1
SujankarthikM <132041297+SujankarthikM@users.noreply.github.com>
2023-04-28 12:05:52 +0530

    Update chatbot.py

--- examples/chatbot.py
+++ examples/chatbot.py
@@ -1,17 +1,37 @@
-import streamlit as st
+import streamlit as st 
+import openai
 from streamlit_chat import message
 import requests
 
-st.set_page_config(
-    page_title="Streamlit Chat - Demo",
-    page_icon=":robot:"
-)
+openai.api_key="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"
 
-API_URL = "https://api-inference.huggingface.co/models/facebook/blenderbot-400M-distill"
-headers = {"Authorization": st.secrets['api_key']}
+def generate_output(prompt):
 
-st.header("Streamlit Chat - Demo")
-st.markdown("[Github](https://github.com/ai-yash/st-chat)")
+
+  res=openai.Completion.create(
+      engine="text-davinci-003",
+      prompt=prompt,
+      temperature=0.5,
+      max_tokens=512
+  )
+  return res.choices[0].text
+
+st.title("WASTE MANAGEMENT")
+def add_bg_from_url():
+    st.markdown(
+         f"""
+         <style>
+         .stApp {{
+             background-image: url("https://c0.wallpaperflare.com/preview/828/635/683/pollution-rubbish-waste-environment.jpg");
+             background-attachment: fixed;
+             background-size: cover
+         }}
+         </style>
+         """,
+         unsafe_allow_html=True
+     )
+
+add_bg_from_url()
 
 if 'generated' not in st.session_state:
     st.session_state['generated'] = []
@@ -19,32 +39,20 @@ if 'generated' not in st.session_state:
 if 'past' not in st.session_state:
     st.session_state['past'] = []
 
-def query(payload):
-	response = requests.post(API_URL, headers=headers, json=payload)
-	return response.json()
-
 def get_text():
-    input_text = st.text_input("You: ","Hello, how are you?", key="input")
+    input_text = st.text_input("You: ","", key="input")
     return input_text 
 
 
 user_input = get_text()
 
 if user_input:
-    output = query({
-        "inputs": {
-            "past_user_inputs": st.session_state.past,
-            "generated_responses": st.session_state.generated,
-            "text": user_input,
-        },"parameters": {"repetition_penalty": 1.33},
-    })
-
+    output=generate_output(user_input)
     st.session_state.past.append(user_input)
-    st.session_state.generated.append(output["generated_text"])
+    st.session_state.generated.append(output)
 
 if st.session_state['generated']:
 
     for i in range(len(st.session_state['generated'])-1, -1, -1):
         message(st.session_state["generated"][i], key=str(i))
         message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
-
