1ab3cecf84419dd5c140719967ebb483b7d8e0a5
SujankarthikM <132041297+SujankarthikM@users.noreply.github.com>
2023-04-28 12:07:48 +0530

    Update chatbot.py

--- examples/chatbot.py
+++ examples/chatbot.py
@@ -3,18 +3,8 @@ import openai
 from streamlit_chat import message
 import requests
 
-openai.api_key="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"
-
-def generate_output(prompt):
 
 
-  res=openai.Completion.create(
-      engine="text-davinci-003",
-      prompt=prompt,
-      temperature=0.5,
-      max_tokens=512
-  )
-  return res.choices[0].text
 
 st.title("WASTE MANAGEMENT")
 def add_bg_from_url():
@@ -31,28 +21,3 @@ def add_bg_from_url():
          unsafe_allow_html=True
      )
 
-add_bg_from_url()
-
-if 'generated' not in st.session_state:
-    st.session_state['generated'] = []
-
-if 'past' not in st.session_state:
-    st.session_state['past'] = []
-
-def get_text():
-    input_text = st.text_input("You: ","", key="input")
-    return input_text 
-
-
-user_input = get_text()
-
-if user_input:
-    output=generate_output(user_input)
-    st.session_state.past.append(user_input)
-    st.session_state.generated.append(output)
-
-if st.session_state['generated']:
-
-    for i in range(len(st.session_state['generated'])-1, -1, -1):
-        message(st.session_state["generated"][i], key=str(i))
-        message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
